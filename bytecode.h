b019a25d026726b5b9a571ebe59691fa4bff8bfe
Aske Simon Christensen <askeksa@users.noreply.github.com>
2014-04-13 14:27:05 +0200

    Bytecode generator

---
+++ bytecode.h
@@ -0,0 +1,80 @@
+#pragma once
+
+#include <string>
+
+static inline int verify(int base, int v, int max, const char *what) {
+	if (v > max) {
+		throw Exception(std::string("Argument out of range for ") + what + " instruction: " + std::to_string(v));
+	}
+	return base + v;
+}
+
+#define BC_DONE  0x00
+#define BC_ELSE  0x01
+#define BC_END   0x02
+#define BC_RAND  0x03
+#define BC_DRAW  0x04
+#define BC_MUL   0x05
+#define BC_SEED  0x06
+#define BC_DIV   0x07
+#define BC_WAIT  0x08
+#define BC_SINE  0x09
+#define BC_MOVE  0x0A
+#define BC_WHEN(cond)  (verify(0x10, cond,  15, "WHEN"))
+#define BC_FORK(nargs) (verify(0x20, nargs, 15, "FORK"))
+#define BC_OP(op)      (verify(0x30, op,    15, "OP"))
+#define BC_WSTATE(i)   (verify(0x40, i,     15, "WSTATE"))
+#define BC_RSTATE(i)   (verify(0x50, i,     15, "RSTATE"))
+#define BC_POP(n)      (verify(0x60, n,     15, "POP"))
+#define BC_LOCAL(i)    (verify(0x70, i,     15, "LOCAL"))
+#define BC_CONST(i)    (verify(0x80, i,    127, "CONST"))
+
+// Negated condition branch nibble
+#define CMP_EQ   6
+#define CMP_NE   7
+#define CMP_LT  12
+#define CMP_GE  13
+#define CMP_LE  14
+#define CMP_GT  15
+
+// Instruction high nibble
+#define OP_OR     8
+#define OP_SUB    9
+#define OP_CMP   11
+#define OP_AND   12
+#define OP_ADD   13
+
+#define ST_X    0
+#define ST_Y    1
+#define ST_SIZE 2
+#define ST_TINT 3
+#define ST_RAND 4
+#define ST_DIR  5
+#define ST_TIME 6
+#define ST_PROC 7
+
+
+typedef unsigned char bytecode_t;
+
+static inline int stack_change(bytecode_t bc) {
+	static const int single[11] = { 0,0,0,1,0,-1,-1,-1,-1,0,-1 };
+	int arg = bc & 15;
+	switch (bc >> 4) {
+	case 0: // Misc
+		return single[bc];
+	case 1: // WHEN
+	case 3: // OP
+	case 4: // WSTATE
+		return -1;
+	case 5: // RSTATE
+	case 7: // LOCAL
+		return 1;
+	case 2: // FORK
+		return -(arg + 1);
+	case 6: // POP
+		return -arg;
+	default: // CONST
+		return 1;
+	}
+}
+
--- translate.cpp
+++ translate.cpp
@@ -4,8 +4,27 @@
 #include "parser.h"
 #include "symbol_linking.h"
 #include "interpret.h"
+#include "code_generator.h"
+
+#include <stdio.h>
 
 using namespace rose;
+
+template <typename T>
+void writefile(std::vector<T> data, const char *filename) {
+	// Convert to big endian
+	for (int i = 0 ; i < data.size() ; i++) {
+		T value = data[i];
+		T swapped = 0;
+		for (int j = 0 ; j < sizeof(T) ; j++) {
+			swapped |= ((value >> (j * 8)) & 0xFF) << ((sizeof(T) - j - 1) * 8);
+		}
+		data[i] = swapped;
+	}
+	FILE *out = fopen(filename, "wb");
+	fwrite(&data[0], sizeof(T), data.size(), out);
+	fclose(out);
+}
 
 std::pair<std::vector<Plot>, std::vector<TintColor>> translate(const char *filename, int max_time, bool print) {
 	std::vector<Plot> plots;
@@ -26,6 +45,26 @@ std::pair<std::vector<Plot>, std::vector<TintColor>> translate(const char *filen
 		}
 		plots = in.interpret(mainproc, max_time);
 		colors = in.get_colors(program);
+
+		// Output
+		CodeGenerator codegen(sl, filename);
+		auto bytecodes_and_constants = codegen.generate(program);
+		std::vector<bytecode_t> bytecodes = bytecodes_and_constants.first;
+		std::vector<number_t> constants = bytecodes_and_constants.second;
+		std::vector<unsigned short> colorscript;
+		int color_frame = -1;
+		for (auto c : colors) {
+			if (c.t != color_frame) {
+				int delta = c.t - color_frame;
+				color_frame = c.t;
+				colorscript.push_back(-delta);
+			}
+			colorscript.push_back(c.rgb | (c.i << 12));
+		}
+		writefile(bytecodes, "bytecodes.bin");
+		writefile(constants, "constants.bin");
+		writefile(colorscript, "colorscript.bin");
+
 	} catch (const CompileException& exc) {
 		if (print) {
 			printf("%s:%d:%d: Error: %s\n", filename, exc.getToken().getLine(), exc.getToken().getPos(), exc.getMessage().c_str());

997a5b452b5deb22c6d849ea2d55bf95dcef67ac
askeksa <askeksa@localhost>
2015-09-13 18:34:38 +0200

    Perform copy pass using textured draw call.

--- shaders.h
+++ shaders.h
@@ -1,4 +1,4 @@
-const char *vshader = R"--(
+const char *plot_vshader = R"--(
 
 uniform vec4 colors[512];
 
@@ -16,13 +16,38 @@ void main() {
 
 )--";
 
-const char *pshader = R"--(
+const char *plot_pshader = R"--(
 
 varying vec2 uv;
 varying vec4 color;
 
 void main() {
 	gl_FragColor = vec4(color.rgb, max(color.a, float(length(uv) < 1.0)));
+}
+
+)--";
+
+const char *quad_vshader = R"--(
+
+attribute vec2 xy;
+
+varying vec2 uv;
+
+void main() {
+	gl_Position = vec4(xy, 0.0, 1.0);
+	uv = (xy + 1.0) * 0.5;
+}
+
+)--";
+
+const char *quad_pshader = R"--(
+
+varying vec2 uv;
+
+uniform sampler2D image;
+
+void main() {
+	gl_FragColor = texture2D(image, uv);
 }
 
 )--";
